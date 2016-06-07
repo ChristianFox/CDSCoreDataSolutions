8f3d8ad499f89be030217aaadbf13edfb27ee734
Christian Fox <christianfox890@icloud.com>
2016-06-07 08:23:36 +0100

    Adds documenting comments to CDSManagedObjectModelDescriptor and CDSPersistentStoreDescriptor

--- CDSCoreDataSolutions/Classes/CDSManagedObjectModelDescriptor.h
+++ CDSCoreDataSolutions/Classes/CDSManagedObjectModelDescriptor.h
@@ -10,10 +10,14 @@
 
 @interface CDSManagedObjectModelDescriptor : NSObject
 
+/// The name of the xcdatamodel file.
 @property (copy,nonatomic) NSString *name;
+/// The directory the xcdatamodel file can be found in. Defaults to NSBundle.mainBundle.
 @property (copy,nonatomic) NSURL *directory;
+/// The URL of the xcdatamodel file. Read only. Created by combining the directory and name.
 @property (copy,nonatomic, readonly) NSURL *URL;
 
+/// Returns a new instance of CDSManagedObjectModelDescriptor.
 +(instancetype)managedObjectModelDescriptor;
 
 @end
--- CDSCoreDataSolutions/Classes/CDSPersistentStoreDescriptor.h
+++ CDSCoreDataSolutions/Classes/CDSPersistentStoreDescriptor.h
@@ -10,14 +10,23 @@
 
 @interface CDSPersistentStoreDescriptor : NSObject
 
+/// The type of store (SQLite, XML etc). Currently only SQLite is supported so defaults to that and is readonly
 @property (copy,nonatomic, readonly) NSString *type;
+/// The name of the store in the file system. Must be set before use.
 @property (copy,nonatomic) NSString *name;
+/// The directory the store should be saved in. Defaults to the Documents Directory.
 @property (copy,nonatomic) NSURL *directory;
+/// The name of the configuration as specified in the xcdatamodel interface.
 @property (copy,nonatomic) NSString *configuration;
+/// The URL of the store file in the file system. Read only, is created by combining the directory and the name properties.
 @property (copy,nonatomic, readonly) NSURL *URL;
+/// The Persistent Store options to be used by the NSPersistentStoreCoordinator.
 @property (copy,nonatomic) NSDictionary *options;
 
+/// Returns an instance of CDSPersistentStoreDescriptor
 +(instancetype)persistentStoreDescriptor;
+
+/// Returns an instance of CDSPersistentStoreDescriptor set with the name.
 +(instancetype)persistentStoreDescriptorWithName:(NSString*)name;
 
 @end
